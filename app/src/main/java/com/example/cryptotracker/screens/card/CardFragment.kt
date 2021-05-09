b9a6626a83a84a2a2c1537c0322355f96358a1a6
ogamoga <cupkek228@gmail.com>
2021-05-09 18:47:23 +0300

    implement navigation

--- app/src/main/java/com/example/cryptotracker/screens/card/CardFragment.kt
+++ app/src/main/java/com/example/cryptotracker/screens/card/CardFragment.kt
@@ -6,15 +6,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.SearchView
+import android.widget.TextView
+import android.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.viewpager.widget.ViewPager
 import com.example.cryptotracker.R
+import com.google.android.material.tabs.TabLayout
+import com.jjoe64.graphview.GraphView
 
 class CardFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CardFragment()
-    }
+    private lateinit var toolbar: Toolbar
+    private lateinit var title: TextView
+    private lateinit var star: CheckBox
+    private lateinit var graph: GraphView
 
-    private lateinit var viewModel: CardViewModel
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +33,21 @@ class CardFragment : Fragment() {
         return inflater.inflate(R.layout.card_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CardViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.initView()
+
+        // place for on click listeners
+
+    }
+
+    private fun View.initView() {
+        toolbar = findViewById(R.id.card_tb)
+        title = findViewById(R.id.card_title)
+        star = findViewById(R.id.card_star)
+        graph = findViewById(R.id.card_graph)
+
+        navController = findNavController()
     }
 
 }
--- app/src/main/java/com/example/cryptotracker/screens/search/SearchFragment.kt
+++ app/src/main/java/com/example/cryptotracker/screens/search/SearchFragment.kt
@@ -5,30 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.ViewPager
 import com.example.cryptotracker.R
+import com.google.android.material.tabs.TabLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var searchView : SearchView
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +28,20 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.initViews()
+
+        // place for click listeners
+
     }
+
+    private fun View.initViews() {
+        searchView = findViewById(R.id.search_sv)
+        tabLayout = findViewById(R.id.search_sliding_tabs)
+        viewPager = findViewById(R.id.search_viewpager)
+
+        navController = findNavController()
+    }
+
 }
